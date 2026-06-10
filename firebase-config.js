0f3387ef39e629fdc0cab8c2ffb1891e7785163a
gabifav <gabifav@outlook.com>
2026-06-10 17:35:02 +1000

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCtcD5gX0RElQd-9NuT3ZHsYLq77VDMiVM",
+  authDomain: "together2-skilltree.firebaseapp.com",
+  projectId: "together2-skilltree",
+  storageBucket: "together2-skilltree.firebasestorage.app",
+  messagingSenderId: "275487629615",
+  appId: "1:275487629615:web:a673c485e8f1655beb01da",
+  measurementId: "G-Z76FXDZM0X"
+};
